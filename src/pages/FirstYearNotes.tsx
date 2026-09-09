b23c99d0e249b1192935f4e3b133cb8c94092cad
Priyal Kumar <230104043@hbtu.ac.in>
2026-09-09 21:21:03 +0000

    feat(first-year): add [2023-24] Complete All PYQs Bundle to chronological PYQ collection

--- src/pages/FirstYearNotes.tsx
+++ src/pages/FirstYearNotes.tsx
@@ -646,7 +646,8 @@ const FirstYearNotes = () => {
         { title: '[2024-25] End Sem PYQs (1st Semester)', url: 'https://drive.google.com/file/d/1FefFp2xjatHyPb205qKyORxsdQKeqVDP/view?usp=drive_link' },
         { title: '[2024-25] Mid Sem-1 PYQs (2nd Semester)', url: 'https://drive.google.com/file/d/1SHThDw8j2ar0oJ7huAKSTTBWAqLqgT86/view?usp=drive_link' },
         { title: '[2024-25] Mid Sem-2 PYQs (2nd Semester)', url: 'https://drive.google.com/file/d/1FmF7U7v0GnqSYhEY5V05Sh2nbq5_Io2V/view?usp=drive_link' },
-        { title: '[2024-25] End Sem PYQs (2nd Semester)', url: 'https://drive.google.com/file/d/1FoxxXrQZyJYqIWNxgqoix-whiELRM9ap/view?usp=drive_link' }
+        { title: '[2024-25] End Sem PYQs (2nd Semester)', url: 'https://drive.google.com/file/d/1FoxxXrQZyJYqIWNxgqoix-whiELRM9ap/view?usp=drive_link' },
+        { title: '[2023-24] Complete All PYQs Bundle', url: 'https://drive.google.com/file/d/1_mrm9SbSkJmqiWuRDT9pwkLAEjKv6e_n/view?usp=drive_link', recommended: true }
       ]
     }
   ];
