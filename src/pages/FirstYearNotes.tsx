f8a7318e5cd96a585bcb9a2756f153ee8e1d6ad9
Priyal Kumar <230104043@hbtu.ac.in>
2026-09-09 21:04:59 +0000

    feat(first-year): add [2025-26] Mid Sem-2 PYQs (Even Semester / 2nd Sem) to chronological PYQ collection

--- src/pages/FirstYearNotes.tsx
+++ src/pages/FirstYearNotes.tsx
@@ -636,7 +636,8 @@ const FirstYearNotes = () => {
         { title: '[2025-26] Mid Sem-1 PYQs (Odd Semester)', url: 'https://drive.google.com/file/d/1UHEYh3ykfe1QtYE_VfOF09nLwfWkjx5Z/view?usp=drivesdk', recommended: true },
         { title: '[2025-26] Mid Sem-2 PYQs (Odd Semester)', url: 'https://drive.google.com/file/d/1VzarRmsgnrELjL9fsGKFy31Ba1G9nZZi/view?usp=drivesdk', recommended: true },
         { title: '[2025-26] End Sem PYQs (Odd Semester)', url: 'https://drive.google.com/file/d/1RvGVTwQiqsc2AcodFj8g7P76_Dy75B_8/view?usp=drivesdk', recommended: true },
-        { title: '[2025-26] Mid Sem-1 PYQs (Even Semester / 2nd Sem)', url: 'https://drive.google.com/file/d/18k4F_9KcyfRkEt-rjsDIMA2wN4BvZj_1/view', recommended: true }
+        { title: '[2025-26] Mid Sem-1 PYQs (Even Semester / 2nd Sem)', url: 'https://drive.google.com/file/d/18k4F_9KcyfRkEt-rjsDIMA2wN4BvZj_1/view', recommended: true },
+        { title: '[2025-26] Mid Sem-2 PYQs (Even Semester / 2nd Sem)', url: 'https://drive.google.com/file/d/1wty3ubMfR4Vyt6ClzFfi0gKi-jNH0EmY/view?usp=drivesdk' }
       ]
     }
   ];
